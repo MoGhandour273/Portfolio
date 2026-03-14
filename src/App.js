9ddc2b4f142540b8e9c6003c6236517e43fad2c1
ghandour <moghandourone1@gmail.com>
2026-03-14 22:29:13 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import NavBar from './components/NavBar';
 import './App.css';
 import HomePage from './components/HomePage';
-import { HashRouter } from "react-router-dom";
 import { useEffect, useState } from "react";
 
 function App() {
